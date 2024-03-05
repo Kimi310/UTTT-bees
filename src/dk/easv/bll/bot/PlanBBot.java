af0cf9a319b78e89364301dc54df8e0dc8c6ea6f
Kimi310 <tymon.mikula@gmail.com>
2024-03-05 18:05:03 +0100

    a little better prototype

--- src/dk/easv/bll/bot/PlanBBot.java
+++ src/dk/easv/bll/bot/PlanBBot.java
@@ -4,7 +4,9 @@ import dk.easv.bll.field.IField;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 import dk.easv.bll.move.Move;
+import javafx.collections.FXCollections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -14,15 +16,18 @@ import static dk.easv.bll.field.IField.AVAILABLE_FIELD;
 public class PlanBBot implements IBot{
     private int depth = 5;
     private int botId;
+    private int opponentId;
     @Override
     public IMove doMove(IGameState state) {
 
         System.out.println(Arrays.deepToString(findMicroBoard(state.getField().getMacroboard(),state)));
         if (state.getMoveNumber()==0){
             botId =0;
+            opponentId=1;
             return new Move(4,4);
         } else if (state.getMoveNumber()==1) {
             botId=1;
+            opponentId=0;
         }
 
         return new Move(0,0);
@@ -33,25 +38,23 @@ public class PlanBBot implements IBot{
         return "PlanBBot";
     }
 
-    private int evaluatePosition(String[][] currentMicroboard, int currentDepth, int turn){
-        if (currentDepth==depth){
-            return 0;
-        }else {
-            for (int i=0;i<3;i++){
-                for (int j=0;j<3;j++){
-                    if (Objects.equals(currentMicroboard[i][j], ".")){
-                        currentMicroboard[i][j] = String.valueOf(turn);
-                        if (turn == 0){
-                            turn=1;
-                        }else {
-                            turn=0;
-                        }
-                        String [][] boards = {{".",".","."},{".",".","."},{".",".","."}};
-                        boards[i][j] = String.valueOf(-1);
+    private IMove evaluatePosition(String[][] currentMicroboard, int currentDepth, boolean isMaximizer,IGameState state){
+        int bestVal = -1000;
+        Move bestMove = new Move(0,0);
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                if (Objects.equals(currentMicroboard[i][j], ".")){
+                    currentMicroboard[i][j]=String.valueOf(botId);
+                    int moveval = minimax(currentMicroboard,false,0,state);
+                    currentMicroboard[i][j]=".";
+                    if (moveval>bestVal){
+                        bestVal = moveval;
+                        bestMove = new Move(i,j);
                     }
                 }
             }
         }
+        return bestMove;
     }
 
     private String[][] findMicroBoard(String[][] boards, IGameState state){
@@ -70,5 +73,41 @@ public class PlanBBot implements IBot{
             }
         }
         return null;
+    }
+
+    private int minimax(String[][] currentMicroboard,boolean isMax,int currentDepth,IGameState state){
+        if (currentDepth == depth){
+            return 0; // here we will determine the state of the board
+        }
+
+        if (isMax){
+            int best = -1000;
+            for (int i = 0;i<3;i++){
+                for (int j=0;j<3;j++){
+                    if (Objects.equals(currentMicroboard[i][j], ".")){
+                        currentMicroboard[i][j]=String.valueOf(botId);
+                        String[][] boards = {{".",".","."},{".",".","."},{".",".","."}};
+                        boards[i][j] = AVAILABLE_FIELD;
+                        best = Math.max(best,minimax(findMicroBoard(boards,state),!isMax,currentDepth+1,state));
+                        currentMicroboard[i][j]=".";
+                    }
+                }
+            }
+            return best;
+        }else {
+            int best = 1000;
+            for (int i = 0;i<3;i++){
+                for (int j=0;j<3;j++){
+                    if (Objects.equals(currentMicroboard[i][j], ".")){
+                        currentMicroboard[i][j]=String.valueOf(opponentId);
+                        String[][] boards = {{".",".","."},{".",".","."},{".",".","."}};
+                        boards[i][j] = AVAILABLE_FIELD;
+                        best = Math.min(best,minimax(findMicroBoard(boards,state),!isMax,currentDepth+1,state));
+                        currentMicroboard[i][j]=".";
+                    }
+                }
+            }
+            return best;
+        }
     }
 }
