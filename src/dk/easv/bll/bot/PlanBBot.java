010be6e0f36c3b5dba451cf43abd53078d214ac1
Kimi310 <tymon.mikula@gmail.com>
2024-03-05 16:54:58 +0100

    prototype of function made to evaluate position (not finished)

--- src/dk/easv/bll/bot/PlanBBot.java
+++ src/dk/easv/bll/bot/PlanBBot.java
@@ -13,12 +13,18 @@ import static dk.easv.bll.field.IField.AVAILABLE_FIELD;
 
 public class PlanBBot implements IBot{
     private int depth = 5;
+    private int botId;
     @Override
     public IMove doMove(IGameState state) {
+
         System.out.println(Arrays.deepToString(findMicroBoard(state.getField().getMacroboard(),state)));
         if (state.getMoveNumber()==0){
+            botId =0;
             return new Move(4,4);
+        } else if (state.getMoveNumber()==1) {
+            botId=1;
         }
+
         return new Move(0,0);
     }
 
@@ -27,10 +33,25 @@ public class PlanBBot implements IBot{
         return "PlanBBot";
     }
 
-    private void evaluatePosition(IGameState state){
-        List<IMove> available_moves = state.getField().getAvailableMoves();
-        String[][] boards = state.getField().getMacroboard();
-
+    private int evaluatePosition(String[][] currentMicroboard, int currentDepth, int turn){
+        if (currentDepth==depth){
+            return 0;
+        }else {
+            for (int i=0;i<3;i++){
+                for (int j=0;j<3;j++){
+                    if (Objects.equals(currentMicroboard[i][j], ".")){
+                        currentMicroboard[i][j] = String.valueOf(turn);
+                        if (turn == 0){
+                            turn=1;
+                        }else {
+                            turn=0;
+                        }
+                        String [][] boards = {{".",".","."},{".",".","."},{".",".","."}};
+                        boards[i][j] = String.valueOf(-1);
+                    }
+                }
+            }
+        }
     }
 
     private String[][] findMicroBoard(String[][] boards, IGameState state){
